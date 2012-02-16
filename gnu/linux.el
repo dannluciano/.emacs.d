80514035e88558f65da55aa85c689748547f8966
Dann Luciano <dannluciano@gmail.com>
2012-02-16 17:07:42 -0300

    Added fix in GNU/Linux

--- gnu/linux.el
+++ gnu/linux.el
@@ -1,6 +1,9 @@
-(message "GNU/Linux")
+;; Textmate
+(require 'textmate)
+
+
 (cua-mode)
-;; (set-default-font "Monospace-14")
+(set-default-font "Monospace-14")
 
 
 (global-set-key (kbd "s-b") 'ibuffer)
@@ -9,7 +12,8 @@
 (global-set-key (kbd "s-2") 'split-window-vertically)
 (global-set-key (kbd "s-3") 'split-window-horizontally)
 (global-set-key (kbd "s-6") 'flymake-mode)
-(global-set-key (kbd "s-;") 'comment-or-uncomment-region-or-line)
+(global-set-key (kbd "s-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "s-k") 'kill-buffer)
 (global-set-key (kbd "<s-up>") 'beginning-of-buffer)
 (global-set-key (kbd "<s-down>") 'end-of-buffer)
 (global-set-key (kbd "<s-right>") 'end-of-line)
@@ -20,7 +24,7 @@
 (global-set-key (kbd "<M-down>") 'forward-paragraph)
 (global-set-key (kbd "<M-s-right>") 'next-buffer)
 (global-set-key (kbd "<M-s-left>") 'previous-buffer)
-
+(global-set-key (kbd "<s-return>") 'toggle-fullscreen)
 
 (defun toggle-fullscreen (&optional f)
   (interactive)
