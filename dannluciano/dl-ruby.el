21584981c06dbe6dfc9462facb6f35c13d7b9386
Dann Luciano <dannluciano@gmail.com>
2012-03-24 22:52:33 -0300

    Removing elpa to git

--- dannluciano/dl-ruby.el
+++ dannluciano/dl-ruby.el
@@ -1,2 +1,11 @@
 (require 'rvm)
 (rvm-use-default)
+
+;; (add-to-list 'load-path (concat dotfiles-dir "/vendor/ruby-complexity/"))
+;; (require 'linum)
+;; (require 'ruby-complexity)
+;; (add-hook 'ruby-mode-hook
+;;           (function (lambda ()
+;;                       (flymake-mode)
+;;                       (linum-mode)
+;;                       (ruby-complexity-mode))))
